d2b4b03e635eeb2e0095714c8c88edf007db9527
Rafael Cardoso <rafael.cardoso@ebanx.com>
2018-12-11 20:05:32 -0200

    User Login

--- src/User/UseCase/UserService.php
+++ src/User/UseCase/UserService.php
@@ -10,4 +10,6 @@ interface UserService
     public function __construct(UserServiceResolver $void);
 
     public function createUser(User $user, string $password): bool;
+
+    public function login(string $username, string $password): bool;
 }
--- src/Vault/Vault.php
+++ src/Vault/Vault.php
@@ -7,8 +7,18 @@ class Vault
 
     public static function encrypt(string $value, string $namespace): string
     {
-        $salt = 'environment';
-        $hash = "{$salt}{$value}{$namespace}";
-        return hash('SHA256', $hash);
+        $hash = self::getPasswordWithNamespace($value, $namespace);
+        return password_hash($hash, PASSWORD_BCRYPT);
+    }
+
+    public static function verifyPassword(string $passwordHash, string $value, string $namespace): bool
+    {
+        $hash = self::getPasswordWithNamespace($value, $namespace);
+        return password_verify($hash, $passwordHash);
+    }
+
+    protected static function getPasswordWithNamespace(string $value, string $namespace): string
+    {
+        return $hash = "{$value}{$namespace}";
     }
 }
